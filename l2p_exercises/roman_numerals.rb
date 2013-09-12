274021aa710fa78f511c963f122de5452fcbdadc
Lee Stetson <kaplejon@gmail.com>
2013-09-12 15:59:18 -0400

    Starting to build up the roman_numerals.rb

--- l2p_exercises/roman_numerals.rb
+++ l2p_exercises/roman_numerals.rb
@@ -1,1 +1,27 @@
-# The task is to create a script that turns integers into their roman numeral equivalents.
+=begin
+	
+ Old-school Roman numerals.  In the early days of Roman numerals, the Romans didn't bother with 
+ any of this new-fangled subtraction "IX" nonsense.  No sir, it was straight addition, biggest 
+ to littlest--so 9 was written "VIIII," and so on.  Write a method that when passed an integer 
+ between 1 and 3000 (or so) returns a string containing the proper oldschool Roman numeral.  In other 
+ words, 'old_roman_numeral 4' should return 'IIII'.  Make sure to test your method on a bunch of 
+ different numbers.  Hint: Use the integer division and modulus methods on page 32.
+ 
+ For reference, these are the values of the letters used:
+ I = 1
+ V = 5
+ X = 10
+ L = 50
+ C = 100
+ D = 500
+ M = 1000
+	
+=end
+
+puts 'Please enter an integer greater than zed.'
+user_number = gets.chomp.to_i
+if user_number <= 0
+	puts 'That is not a valid integer.'
+else
+	puts "Nice job entering #{user_number}"
+end
